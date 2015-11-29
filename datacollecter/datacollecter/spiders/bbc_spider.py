9d18aaaa901fb3c762fde252bff73cc325b075d9
Patrick Murphy <murph.pm@gmail.com>
2015-11-28 21:42:05 -0600

    Added following and parsing links to bbc spider

--- datacollecter/datacollecter/spiders/bbc_spider.py
+++ datacollecter/datacollecter/spiders/bbc_spider.py
@@ -5,29 +5,60 @@ import re
 class BbcSpider(scrapy.Spider):
     # Name must be specified outside of the constructor in order to be able to crawl using this spider
     name = "bbc"
+    max_articles = 2000
     
     def __init__(self, *args):
         self.allowed_domains = ["bbc.com", "bbc.co.uk"]
         self.start_urls = ["http://www.bbc.com"]
+        self.news_re = re.compile('.*\/news\/.*')
+        # Ignore specific links
+        self.invalid_res = [
+            re.compile('.*comments.*'),
+            re.compile('.*\/live\/.*'),
+            re.compile('.*\/blogs\/.*')
+        ]
         self.supported_news_domains = {
-            re.compile('.*news\/world-.*'): self.parse_world_article
+            self.news_re: self.parse_world_article
         }
+        
+        self.articles_seen = []
+        self.article_count = 0
     
         scrapy.Spider.__init__(self, *args)
     
     def parse(self, response):
-        articles_on_page = response.xpath("//a[@class = 'block-link__overlay-link']//@href")
+        articles_on_page = response.xpath("//a//@href")
         for article_url in articles_on_page:
-            full_article_url = response.urljoin(article_url.extract())
-            yield scrapy.Request(full_article_url, callback=self.parse_article)
+            if self.news_re.match(article_url.extract()):
+                full_article_url = response.urljoin(article_url.extract())
+                yield scrapy.Request(full_article_url, callback=self.parse_article)
     
     def parse_article(self, response):
         # Only works for specific sections
-        for supported_news_domain in self.supported_news_domains:
-            if supported_news_domain.match(response.url):
-                article_item = self.supported_news_domains[supported_news_domain](response)
-                if article_item:
-                    yield article_item
+        if response.url not in self.articles_seen and self.crawler.stats.get_value('pages_crawled') < self.max_articles:
+            for supported_news_domain in self.supported_news_domains:
+                if supported_news_domain.match(response.url):
+                    article_item = self.supported_news_domains[supported_news_domain](response)
+                    if article_item and response.url not in self.articles_seen:
+                        self.crawler.stats.inc_value('pages_crawled')
+                        yield article_item
+        
+            articles_on_page = response.xpath("//a//@href")
+            for article_url in articles_on_page:
+                if self.news_re.match(article_url.extract()):
+                    invalid = False
+                    for invalid_re in self.invalid_res:
+                        if invalid_re.match(article_url.extract()):
+                            invalid = True
+                            break
+                            
+                    if invalid or article_url.extract() in self.articles_seen:
+                        continue
+                            
+                    if self.crawler.stats.get_value('pages_crawled') < self.max_articles:
+                        self.articles_seen.append(response.url)
+                        full_article_url = response.urljoin(article_url.extract())
+                        yield scrapy.Request(full_article_url, callback=self.parse_article)
         
     def parse_world_article(self, response):
         title = response.xpath("//h1[@class = 'story-body__h1']//text()[1]").extract()
@@ -40,5 +71,6 @@ class BbcSpider(scrapy.Spider):
         article_item = datacollecter.items.ArticleItem()
         article_item['title'] = title[0]
         article_item['text'] = text
+        article_item['url'] = response.url
         return article_item
         
